a749eeee2de4e351bb8d0559d18e9085327e713a
ogagaoghene emmanuel <esavwede84@gmail.com>
2024-09-01 15:13:23 +0100

    added server graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,53 @@ import './pre-start'; // Must be the first import
 import logger from 'jet-logger';
 
 import EnvVars from '@src/common/EnvVars';
-import server from './server';
+import app from './server';
+import { Server, createServer}  from "http" 
 
 
 // **** Run **** //
 
+const server: Server = createServer(app)
 const SERVER_START_MSG = ('Express server started on port: ' + 
   EnvVars.Port.toString());
 
+
+
 server.listen(EnvVars.Port, () => logger.info(SERVER_START_MSG));
+
+
+
+
+process.on("SIGINT", ()=>{  gracefulShutdown() } ) 
+process.on("SIGTERM", ()=>{ gracefulShutdown() } )
+
+
+
+function gracefulShutdown()
+{
+  try 
+  {
+      console.log(' Attempting Graceful Shutdown ')
+
+      server.close(( e?: Error )=>{
+
+          if( e )
+          {
+            console.log("Error occured while closing the server ")
+            console.log(e)
+          }
+
+          // Shut down other resources 
+          console.log(" Graceful Shutdown Successful ")
+          process.exit(0) 
+      })
+
+
+  }
+  catch(e)
+  {
+    console.log("Error occured while attempting graceful shutdown ") 
+    console.log(e)
+  }
+}
+
