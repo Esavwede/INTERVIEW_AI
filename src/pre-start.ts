bc8ecbf851b7f6dbe0380c0c1eba5c75219591ed
ogagaoghene emmanuel <esavwede84@gmail.com>
2024-09-05 14:04:47 +0100

    fixed env issue

--- src/pre-start.ts
+++ src/pre-start.ts
@@ -30,10 +30,8 @@ const args = parse<IArgs>({
 
 // Set the env file
 const result2 = dotenv.config({
-  path: `./${args.env}.env`,
+  path: path.join(__dirname, `../env/${args.env}.env`),
 });
-
-
 if (result2.error) {
   throw result2.error;
 }
