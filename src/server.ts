ecdf08e92c56cdfdaddeaf3811911958e12cea46
ogagaoghene emmanuel <esavwede84@gmail.com>
2024-09-15 17:13:54 +0100

    feature: added sentry monitoring

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,18 @@
  * Setup express server.
  */
 
+
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+import { config } from "dotenv"
+config() 
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import path from 'path';
 import helmet from 'helmet';
 import express, { Request, Response, NextFunction } from 'express';
 import logger from 'jet-logger';
-import cors from "cors" 
 
 import 'express-async-errors';
 
@@ -20,20 +25,23 @@ import { NodeEnvs } from '@src/common/misc';
 import { routes } from './routes';
 
 
-// **** Variables **** //
+
+
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN ,
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
 
 const app = express();
-
-
-// **** Setup **** //
-
-
-// app.use(cors({
-//   origin: 'http://localhost:3000',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
-
 
 // Basic middleware
 app.use(express.json());
@@ -58,6 +66,9 @@ if (EnvVars.NodeEnv === NodeEnvs.Production.valueOf()) {
 // Add APIs, must be after middleware
 routes(app) 
 
+// Sentry Error Monitoring 
+Sentry.setupExpressErrorHandler(app); 
+
 // Add error handler
 app.use((
   err: Error,
@@ -77,11 +88,12 @@ app.use((
 });
 
 
+
 // **** Front-End Content **** //
 
-// Set views directory (html)
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
+// // Set views directory (html)
+// const viewsDir = path.join(__dirname, 'views');
+// app.set('views', viewsDir);
 
 // Set static directory (js and css).
 const staticDir = path.join(__dirname, 'public');
@@ -89,12 +101,13 @@ app.use(express.static(staticDir));
 
 // Nav to users pg by default
 app.get('/', (_: Request, res: Response) => {
-  return res.redirect('/users');
+  return res.status(200).json({ success: true, "msg":"Welcome to the Interview AI API"})
 });
 
 // Redirect to login if not logged in.
 app.get('/users', (_: Request, res: Response) => {
-  return res.sendFile('users.html', { root: viewsDir });
+  throw new Error("Sample Error") 
+  return res.status(200).json({ success: true, "msg":"Welcome to the Interview AI API"})
 });
 
 
