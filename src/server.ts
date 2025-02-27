807bb287e40a68938566a3540157e035e7c6c48e
ogagaoghene emmanuel <esavwede84@gmail.com>
2025-02-27 16:11:48 +0100

    feature: allowed access from all origins in development

--- src/server.ts
+++ src/server.ts
@@ -39,7 +39,7 @@ app.use(cookieParser(EnvVars.CookieProps.Secret))
 
 // Cors 
 app.use(cors({
-  origin: ["http://localhost:3000","http://localhost:5173","https://interviewaiafrotech.netlify.app"], // Allows clear all origins, you can restrict this to specific origins
+  origin: "*",
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: false 
 }));
